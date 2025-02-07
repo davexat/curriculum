462c8659eccbf9c68a3aee5a3cfceee279548f69
davexat <yisuscristh20@gmail.com>
2025-02-06 22:59:50 -0500

    fix: meta charset

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <Head>
         <title>David Sandoval - Curriculum Vitae</title>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta
           name="keywords"
           content="web development, python, javascript, backend, frontend, programming, software design, software development, software engineering, computer science, engineering"
