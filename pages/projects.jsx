c6a136415c4c10fcbf245c2ca090cbd567e62e1d
davexat <yisuscristh20@gmail.com>
2025-02-06 21:34:14 -0500

    chore: tech component

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,12 +1,13 @@
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import TechItem from '../components/TechItem';
 
 export default function Projects() {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Permissions-Policy" content="interest-cohort=()" />
         <title>Projects - David Sandoval</title>
@@ -18,112 +19,24 @@ export default function Projects() {
         <section id="skills" className="skills-section section">
           <h2 className="section-heading">Skills and Technologies</h2>
           <div className="tech-list">
-            <div className="tech-item">
-              <a href="https://www.python.org/" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/python.png" alt="Python" />
-                <p className="tech-text">Python</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://www.mysql.com/" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/mysql.png" alt="MySQL" />
-                <p className="tech-text">MySQL</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://pandas.pydata.org/" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/pandas.png" alt="Pandas" />
-                <p className="tech-text">Pandas</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://numpy.org/" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/numpy.png" alt="Numpy" />
-                <p className="tech-text">Numpy</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://jupyter.org/" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/jupyter.png" alt="Jupyter" />
-                <p className="tech-text">Jupyter</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://scikit-learn.org/" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/scikitlearn.png" alt="ScikitLearn" />
-                <p className="tech-text">ScikitLearn</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://streamlit.io/" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/streamlit.png" alt="Streamlit" />
-                <p className="tech-text">Streamlit</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://www.java.com/" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/java.png" alt="Java" />
-                <p className="tech-text">Java</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://junit.org/junit5/" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/junit5.png" alt="JUnit" />
-                <p className="tech-text">JUnit</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://developer.mozilla.org/es/docs/Web/JavaScript" target="_blank"
-                rel="noopener noreferrer">
-                <img src="/icons/techs/javascript.png" alt="JavaScript" />
-                <p className="tech-text">JavaScript</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank"
-                rel="noopener noreferrer">
-                <img src="/icons/techs/html.png" alt="HTML" />
-                <p className="tech-text">HTML</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"
-                rel="noopener noreferrer">
-                <img src="/icons/techs/css.png" alt="CSS" />
-                <p className="tech-text">CSS</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://sass-lang.com/" target="_blank"
-                rel="noopener noreferrer">
-                <img src="/icons/techs/sass.png" alt="Sass" />
-                <p className="tech-text">Sass</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/git.png" alt="Git" />
-                <p className="tech-text">Git</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/github-b.png" alt="GitHub" />
-                <p className="tech-text">GitHub</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://daringfireball.net/projects/markdown/" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/markdown.png" alt="Markdown" />
-                <p className="tech-text">Markdown</p>
-              </a>
-            </div>
-            <div className="tech-item">
-              <a href="https://en.cppreference.com/w/c/language" target="_blank" rel="noopener noreferrer">
-                <img src="/icons/techs/c.png" alt="C" />
-                <p className="tech-text">C</p>
-              </a>
-            </div>
+            <TechItem url="https://www.python.org/" imageUrl="/icons/techs/python.png" name="Python" />
+            <TechItem url="https://www.mysql.com/" imageUrl="/icons/techs/mysql.png" name="MySQL" />
+            <TechItem url="https://pandas.pydata.org/" imageUrl="/icons/techs/pandas.png" name="Pandas" />
+            <TechItem url="https://numpy.org/" imageUrl="/icons/techs/numpy.png" name="Numpy" />
+            <TechItem url="https://jupyter.org/" imageUrl="/icons/techs/jupyter.png" name="Jupyter" />
+            <TechItem url="https://scikit-learn.org/" imageUrl="/icons/techs/scikitlearn.png" name="ScikitLearn" />
+            <TechItem url="https://streamlit.io/" imageUrl="/icons/techs/streamlit.png" name="Streamlit" />
+            <TechItem url="https://www.java.com/" imageUrl="/icons/techs/java.png" name="Java" />
+            <TechItem url="https://junit.org/junit5/" imageUrl="/icons/techs/junit5.png" name="JUnit" />
+            <TechItem url="https://developer.mozilla.org/es/docs/Web/JavaScript" imageUrl="/icons/techs/javascript.png" name="JavaScript" />
+            <TechItem url="https://developer.mozilla.org/en-US/docs/Web/HTML" imageUrl="/icons/techs/html.png" name="HTML" />
+            <TechItem url="https://developer.mozilla.org/en-US/docs/Web/CSS" imageUrl="/icons/techs/css.png" name="CSS" />
+            <TechItem url="https://sass-lang.com/" imageUrl="/icons/techs/sass.png" name="Sass" />
+            <TechItem url="https://es.react.dev/" imageUrl="/icons/techs/react.png" name="React" />
+            <TechItem url="https://git-scm.com/" imageUrl="/icons/techs/git.png" name="Git" />
+            <TechItem url="https://github.com/" imageUrl="/icons/techs/github-b.png" name="GitHub" />
+            <TechItem url="https://daringfireball.net/projects/markdown/" imageUrl="/icons/techs/markdown.png" name="Markdown" />
+            <TechItem url="https://en.cppreference.com/w/c/language" imageUrl="/icons/techs/c.png" name="C" />
           </div>
         </section>
         <section id="projects-personal" className="section">
@@ -139,49 +52,13 @@ export default function Projects() {
                 <li>Helps compare customer segments and optimize business strategies.</li>
               </ul>
               <div className="project-technologies">
-                <div className="mini-tech">
-                  <a href="https://www.python.org/" target="_blank" rel="noopener noreferrer">
-                    <img src="/icons/techs/python.png" alt="Python" />
-                    <p className="mini-tech-text">Python</p>
-                  </a>
-                </div>
-                <div className="mini-tech">
-                  <a href="https://pandas.pydata.org/" target="_blank" rel="noopener noreferrer">
-                    <img src="/icons/techs/pandas.png" alt="Pandas" />
-                    <p className="mini-tech-text">Pandas</p>
-                  </a>
-                </div>
-                <div className="mini-tech">
-                  <a href="https://numpy.org/" target="_blank" rel="noopener noreferrer">
-                    <img src="/icons/techs/numpy.png" alt="Numpy" />
-                    <p className="mini-tech-text">Numpy</p>
-                  </a>
-                </div>
-                <div className="mini-tech">
-                  <a href="https://jupyter.org/" target="_blank" rel="noopener noreferrer">
-                    <img src="/icons/techs/jupyter.png" alt="Jupyter" />
-                    <p className="mini-tech-text">Jupyter</p>
-                  </a>
-                </div>
-                <div className="mini-tech">
-                  <a href="https://scikit-learn.org/" target="_blank" rel="noopener noreferrer">
-                    <img src="/icons/techs/scikitlearn.png" alt="ScikitLearn" />
-                    <p className="mini-tech-text">ScikitLearn</p>
-                  </a>
-                </div>
-                <div className="mini-tech">
-                  <a href="https://streamlit.io/" target="_blank" rel="noopener noreferrer">
-                    <img src="/icons/techs/streamlit.png" alt="Streamlit" />
-                    <p className="mini-tech-text">Streamlit</p>
-                  </a>
-                </div>
-                <div className="mini-tech">
-                  <a href="https://daringfireball.net/projects/markdown/" target="_blank"
-                    rel="noopener noreferrer">
-                    <img src="/icons/techs/markdown.png" alt="Markdown" />
-                    <p className="mini-tech-text">Markdown</p>
-                  </a>
-                </div>
+                <TechItem isMini url="https://www.python.org/" imageUrl="/icons/techs/python.png" name="Python" />
+                <TechItem isMini url="https://pandas.pydata.org/" imageUrl="/icons/techs/pandas.png" name="Pandas" />
+                <TechItem isMini url="https://numpy.org/" imageUrl="/icons/techs/numpy.png" name="Numpy" />
+                <TechItem isMini url="https://jupyter.org/" imageUrl="/icons/techs/jupyter.png" name="Jupyter" />
+                <TechItem isMini url="https://scikit-learn.org/" imageUrl="/icons/techs/scikitlearn.png" name="ScikitLearn" />
+                <TechItem isMini url="https://streamlit.io/" imageUrl="/icons/techs/streamlit.png" name="Streamlit" />
+                <TechItem isMini url="https://daringfireball.net/projects/markdown/" imageUrl="/icons/techs/markdown.png" name="Markdown" />
               </div>
               <div className="project-links">
                 <a href="https://github.com/davexat/Customer_Segmentation_Dashboard" target="_blank" rel="noopener noreferrer">View code</a>
@@ -198,34 +75,10 @@ export default function Projects() {
                 <li>Includes sections for about me, projects, and contact information.</li>
               </ul>
               <div className="project-technologies">
-                <div className="mini-tech">
-                  <a href="https://developer.mozilla.org/es/docs/Web/JavaScript" target="_blank"
-                    rel="noopener noreferrer">
-                    <img src="/icons/techs/javascript.png" alt="JavaScript" />
-                    <p className="mini-tech-text">JavaScript</p>
-                  </a>
-                </div>
-                <div className="mini-tech">
-                  <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank"
-                    rel="noopener noreferrer">
-                    <img src="/icons/techs/html.png" alt="HTML" />
-                    <p className="mini-tech-text">HTML</p>
-                  </a>
-                </div>
-                <div className="mini-tech">
-                  <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"
-                    rel="noopener noreferrer">
-                    <img src="/icons/techs/css.png" alt="CSS" />
-                    <p className="mini-tech-text">CSS</p>
-                  </a>
-                </div>
-                <div className="mini-tech">
-                  <a href="https://sass-lang.com/" target="_blank"
-                    rel="noopener noreferrer">
-                    <img src="/icons/techs/sass.png" alt="Sass" />
-                    <p className="mini-tech-text">Sass</p>
-                  </a>
-                </div>
+                <TechItem isMini url="https://developer.mozilla.org/es/docs/Web/JavaScript" imageUrl="/icons/techs/javascript.png" name="JavaScript" />
+                <TechItem isMini url="https://developer.mozilla.org/en-US/docs/Web/HTML" imageUrl="/icons/techs/html.png" name="HTML" />
+                <TechItem isMini url="https://developer.mozilla.org/en-US/docs/Web/CSS" imageUrl="/icons/techs/css.png" name="CSS" />
+                <TechItem isMini url="https://sass-lang.com/" imageUrl="/icons/techs/sass.png" name="Sass" />
               </div>
               <div className="project-links">
                 <a href="https://github.com/davexat/curriculum" target="_blank" rel="noopener noreferrer">View code</a>
