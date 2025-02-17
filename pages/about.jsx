43517a53863c7399340de2fbc0ee98384168d730
davexat <yisuscristh20@gmail.com>
2025-02-16 22:40:11 -0500

    chore: editing about me

--- pages/about.jsx
+++ pages/about.jsx
@@ -26,29 +26,50 @@ export default function About() {
         </section>
         <section id="my-journey" className="about-section section">
           <h2 className="section-heading">My Journey</h2>
-          <p className="section-paragraph">
-            I began my journey in computer science when I discovered PSeint. I
-            was fascinated by everything I could create with this tool, which
-            eventually led me to Python, the first programming language I
-            learned, and I was truly amazed. This experience inspired me to
-            pursue the degree I am currently studying. As I progressed, I
-            discovered my true passion: Software Design, Development, and
-            Artificial Intelligence.
-          </p>
-          <p className="section-paragraph">
-            In 2023, I began studying Computer Science Engineering at Escuela 
-            Superior Politécnica del Litoral (ESPOL). At the end of my first 
-            semester, I participated in PyWeekend, a university-organized 
-            hackathon for Programming Fundamentals students.
-          </p>
-          <p className="section-paragraph">
-            By mid-2024, I had the opportunity to work as a Teaching Assistant 
-            for the Software Design course, one of the many subjects that have 
-            fascinated me. Later that year, I embarked on a project to join the 
-            Politécnico Artificial Intelligence Club (CIAP) under the mentorship 
-            of Kevin Santana. Thanks to his excellent guidance, my teammate 
-            José Toapanta and I earned the Best Aspiring Team award.
-          </p>
+          <div className="about-subsection">
+            <div className="about-text left">
+              <p className="section-paragraph">
+                I began my journey in computer science when I discovered PSeint.
+                I was fascinated by everything I could create with this tool,
+                which eventually led me to Python, the first programming language
+                I learned, and I was truly amazed. This experience inspired me to
+                pursue the degree I am currently studying. As I progressed, I
+                discovered my true passion: Software Design, Development, and
+                Artificial Intelligence.
+              </p>
+              <p className="section-paragraph">
+                In 2023, I began studying Computer Science Engineering at Escuela
+                Superior Politécnica del Litoral (ESPOL). At the end of my first
+                semester, I participated in PyWeekend, a university-organized
+                hackathon for Programming Fundamentals students.
+              </p>
+            </div>
+            <div className="about-image right">
+              <img src="/images/me-in-espol.jpg"/>
+            </div>
+          </div>
+          <div className="about-subsection">
+            <div className="about-text right">
+              <p className="section-paragraph">
+              By mid-2024, I had the opportunity to work as a Teaching Assistant 
+              for the Software Design course, one of the many subjects that have 
+              fascinated me due to its blend of creativity and logical problem-solving. 
+              In this role, I refined my understanding of object-oriented principles, 
+              software architecture, and design patterns. I also developed a passion 
+              for mentoring students, helping them break down complex concepts into 
+              actionable steps, and fostering an environment of collaborative learning.
+              </p>
+              <p className="section-paragraph">
+              Later that year, I embarked on a project to join the 
+              Club de Inteligencia Artificial Politécnico (CIAP) under the mentorship 
+              of Kevin Santana. Thanks to his excellent guidance, my teammate 
+              José Toapanta and I earned the Best Aspiring Team award.
+              </p>
+            </div>
+            <div className="about-image left">
+            <img src="/images/pub2.jpg"/>
+            </div>
+          </div>
           <p className="section-paragraph">
             In 2025... the journey continues! 🚀
           </p>
